47eb5e39587dab316c0ed0b72a60ed486551197a
Chris Mannel <cmannel77@gmail.com>
2015-06-03 21:00:38 -0500

    Created separate files for docs

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myId").style.fontWeight = "bold";
+console.log("I'm logging this information");
+
+document.getElementById("mySpan").onclick = function () {
+  alert('This is a test');
+  };
